b81443749edd292a97e17dcd0a55ee65211d6964
“hjl00 <624544174@qq.com>
2020-05-19 18:19:32 +0800

    注释冹

---
+++ src/test/java/com/hjl/SpringbootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
